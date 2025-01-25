8c17b2f2010ce24647d7b441f9999704f9313abc
riteshbenjwal <riteshbenjwal7@gmail.com>
2025-01-25 16:06:39 +0530

    add basic logic handling for changing temperatur

--- voice-assistant-agent/main.py
+++ voice-assistant-agent/main.py
@@ -1,21 +1,24 @@
 import asyncio
 
 from dotenv import load_dotenv
-from livekit.agents import  AutoSubscribe, JobContext, WorkerOptions, cli, llm
+from livekit.agents import AutoSubscribe, JobContext, WorkerOptions, cli, llm
 from livekit.agents.voice_assistant import VoiceAssistant
 from livekit.plugins import openai, silero
+from api import AssistantFnc
 
 load_dotenv()
+
 
 async def entrypoint(ctx: JobContext):
     initial_ctx = llm.ChatContext().append(
         role="system",
         text=(
-              "You are a voice assistant created by LiveKit. Your interface with users will be voice. "
-              "You should use short and concise responses, and avoiding usage of unpronouncable punctuation."
+            "You are a voice assistant created by LiveKit. Your interface with users will be voice. "
+            "You should use short and concise responses, and avoiding usage of unpronouncable punctuation."
         ),
     )
     await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
+    fnc_ctx = AssistantFnc()
 
     assitant = VoiceAssistant(
         vad=silero.VAD.load(),
@@ -23,12 +26,13 @@ async def entrypoint(ctx: JobContext):
         llm=openai.LLM(),
         tts=openai.TTS(),
         chat_ctx=initial_ctx,
+        fnc_ctx=fnc_ctx,
     )
-
     assitant.start(ctx.room)
 
     await asyncio.sleep(1)
     await assitant.say("Hey, how can I help you today!", allow_interruptions=True)
+
 
 if __name__ == "__main__":
     cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint))
